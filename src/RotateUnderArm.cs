afcdb759418d9e58c6de450c7ef3f6404a163d81
Mika Kruckenberg <71791126+Molukkenberg@users.noreply.github.com>
2023-06-19 16:59:10 +0200

    Create RotateUnderArm.cs

---
+++ src/RotateUnderArm.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class RotateUnderArm : MonoBehaviour
+{
+    private readonly float maxElbowRot = .7f;
+    
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            if(transform.localRotation.x <= maxElbowRot)
+                transform.Rotate(Vector3.right, 1);
+        }
+        else if (Input.GetKey("s"))
+        {
+            if (transform.localRotation.x >= -maxElbowRot)
+                transform.Rotate(Vector3.right, -1);
+        }
+    }
+}
